07c4ff815617473471eeab0a4a298aae66b2be77
Karina Kolos <karina.kolos@mail.ru>
2022-07-13 19:49:10 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ module.exports = function reverse(n) {
     return Number(str.split("").reverse().join(""));
   } else {
     let numb = n * -1;
-    return "-" + Number(str.split("").shift().reverse().join(""));
+    return "-" + Number(numb.split("").shift().reverse().join(""));
   }
 };
